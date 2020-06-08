efa801c1a18a2ffadcd06b13953894211c4f519d
rookie <yaoqi@wanhengtech.com>
2020-06-08 22:10:44 +0800

    代码同步

--- mall-admin/src/main/java/com/rookieyao/admin/MallAdminApplication.java
+++ mall-admin/src/main/java/com/rookieyao/admin/MallAdminApplication.java
@@ -1,13 +1,13 @@
-package com.rookieyao.registry;
+package com.rookieyao.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MallRegistryApplication {
+public class MallAdminApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MallRegistryApplication.class, args);
+        SpringApplication.run(MallAdminApplication.class, args);
     }
 
 }

64e09b875f885704e9c40973e860efb3d3c0db7a
rookie <yaoqi@wanhengtech.com>
2020-06-08 21:58:11 +0800

    first commit

---
+++ mall-registry/src/main/java/com/rookieyao/registry/MallRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.rookieyao.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallRegistryApplication.class, args);
+    }
+
+}
---
+++ mall-registry/src/test/java/com/rookieyao/registry/MallRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rookieyao.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

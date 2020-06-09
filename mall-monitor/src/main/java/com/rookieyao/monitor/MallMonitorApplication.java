09b968588e257b4377fe2e4bd33d200fc669586f
rookie <yaoqi@wanhengtech.com>
2020-06-09 22:43:24 +0800

    几个主要服务修改临时提交

--- mall-monitor/src/main/java/com/rookieyao/monitor/MallMonitorApplication.java
+++ mall-monitor/src/main/java/com/rookieyao/monitor/MallMonitorApplication.java
@@ -1,8 +1,12 @@
 package com.rookieyao.monitor;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
+@EnableAdminServer
 @SpringBootApplication
 public class MallMonitorApplication {
 
